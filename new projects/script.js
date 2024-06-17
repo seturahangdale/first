54b461b345d7afd85a9a725d547dfd01f481c5ad
seturahangdale <seturahangdale@gmail.com>
2024-06-17 15:13:36 +0530

    first commit

---
+++ new projects/script.js
@@ -0,0 +1,810 @@
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+//var arr = [11,22,33,44,55,66,77,88,99]
+//for (var a =arr.length-1; a>=0;a--){
+ //    console.log(arr[a]);
+  //   }
+  
+//1)make  a variable user and inialize a value"ram" and print it on console
+ // var user = "ram";
+ // console.log(user);
+
+  //2)make two variables and print their sum,mul,div,and sub
+ // var a =10;
+ // var b =20;
+   //console.log(a+b,a*b,a/b,a-b);
+
+
+//    function areaOfsquare(a){
+//    console.log(a*a)
+
+//    }
+//    areaOfsquare(5)
+
+//   function add (a,b){
+//     console.log(a+b)
+//   }
+// add(10,5)
+
+// function cube(c){
+//   console.log(c*c*c)
+// }
+// cube(9)
+
+
+// var abc=function(){
+//   console.log("hello")
+//   return 10
+// }
+// console.log(abc())
+
+// var cloths = [
+  // {
+      //  brand:"zara",
+      //  price:10000,
+      //  color:"black",
+
+  
+  // },{
+    // brand:"H&M",
+    // price:5000,
+    // color:"white",
+  // },{
+    // brand:"allen solly",
+    // price:2000,
+    // color:"Blue",
+  // }
+// ]
+// console.log(cloths[2].brand)
+
+var body = document.querySelector("body");
+var cursor = document.querySelector("#cursor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
